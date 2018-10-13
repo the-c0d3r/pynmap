91279eb37202ba2c8a87d88d7a52509c8c8512e1
the-c0d3r <z3r0.mhu@gmail.com>
2018-10-13 13:51:12 +0800

    pingsweep.py: working version

--- lib/ip.py
+++ lib/ip.py
@@ -7,7 +7,7 @@ import socket
 import struct
 import fcntl
 import ipaddress
-from typing import List, Optional
+from typing import List
 
 from lib.colors import Colors
 
@@ -27,9 +27,9 @@ def ip2int(row) -> int:
     return int(ipaddress.IPv4Address(row[0]))
 
 
-def getLocalip(interface="wlan0") -> str:
+def getLocalip(interface: str = "wlan0") -> str:
     """This function will return the Local IP Address of the interface"""
-    if 'nux' in sys.platform:
+    if "nux" in sys.platform:
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         try:
             return socket.inet_ntoa(
@@ -41,5 +41,5 @@ def getLocalip(interface="wlan0") -> str:
             print("{}[!] Error, unable to detect local ip address.".format(Colors.FAIL))
             print("[!] Check your connection to network {}".format(Colors.ENDC))
             exit()
-    elif 'darwin' in sys.platform:
+    elif "darwin" in sys.platform:
         return [ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][0]
--- pingsweep.py
+++ pingsweep.py
@@ -3,14 +3,16 @@
 """
 This is the new ping sweeper to sweep the whole network
 """
-from terminaltables import SingleTable
+import argparse
+import time
 import concurrent.futures
+from terminaltables import SingleTable
+
 from lib.ip import IPV4Network, ip2int
 from lib.scanner import Scanner
 
 
-
-def tableprinter(data):
+def tableprinter(data: [(str, str)]) -> None:
     """Accepts a list of sorted and preformatted data array"""
     sort = sorted(data, key=ip2int)
     data = [("IP", "HOSTNAME")] + sort
@@ -18,13 +20,29 @@ def tableprinter(data):
     print(table.table)
 
 
+def build_parser() -> argparse.ArgumentParser:
+    """Configures a parser and return it"""
+    parser = argparse.ArgumentParser(prog="pingsweeper", description="Multithreaded ping sweeper")
+    parser.add_argument("-t", "--target", help="Target network in CIDR format eg. 192.168.1.0/24")
+    return parser
+
+
 def main():
-    ipList = IPV4Network("192.168.1.0/24").getIPs()
+    parser = build_parser()
+    args = parser.parse_args()
+    if args.target is None:
+        parser.print_help()
+        print("[-] -t target parameter required")
+        exit()
+
+    starttime = int(time.time())
+    ipList = IPV4Network(args.target).getIPs()
     scanner = Scanner()
+    print("Scanning {} network".format(args.target))
 
     scanResult = []
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=254) as executor:
         futures = {executor.submit(scanner.scan, ip, "-sP"): ip for ip in ipList}
 
     for future in concurrent.futures.as_completed(futures):
@@ -35,6 +53,8 @@ def main():
             scanResult.append((host, hostname))
 
     tableprinter(scanResult)
+    endtime = int(time.time())
+    print("Elasped : {} seconds".format(endtime - starttime))
 
 main()
 
